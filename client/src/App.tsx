680f596eae75bc83dddca594c95a38e2753524bd
igaue-takahhiko <hasewo.2610@gmail.com>
2021-06-05 17:37:25 +0900

    setup client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
 
-function App() {
+import PageRender from './PageRender';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Switch>
+        <Route exact path="/" component={PageRender} />
+        <Route exact path="/:page" component={PageRender} />
+        <Route exact path="/:page/:slug" component={PageRender} />
+      </Switch>
     </div>
   );
 }
---
+++ client/src/PageRender.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useParams } from 'react-router-dom';
+import { NotFound } from './components/global';
+import { Params } from './utils/types';
+
+const generatePage = (name: string) => {
+  const component = () => require(`./pages/${name}`).default
+
+  try {
+    return React.createElement(component())
+  } catch (error) {
+    return <NotFound />
+  }
+}
+
+const PageRender = () => {
+  const { page, slug }: Params = useParams()
+
+  let name = ''
+
+  if (page) {
+    name = slug ? `${page}/[slug]` : `${page}`
+  }
+
+  return generatePage(name)
+}
+
+export default PageRender
