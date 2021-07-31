cb10f9f8e85f8e31d25424aacd2aa4fdd9cb5960
igaue-takahhiko <hasewo.2610@gmail.com>
2021-07-31 18:17:39 +0900

    implement home page

--- client/src/components/cards/index.ts
+++ client/src/components/cards/index.ts
@@ -1,2 +1,3 @@
 export { default as CardHorizontal } from "./CardHorizontal"
+export { default as CardVertically } from "./CardVertically"
 export { default as CreateForm } from "./CreateForm"
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react"
+import { useSelector } from "react-redux"
+import { Link } from "react-router-dom"
+
+import { RootStore } from "../utils/globalTypes"
+
+import { CardVertically } from "../components/cards"
 
 const Home: React.FC = () => {
+  const { blogs } = useSelector((state: RootStore) => state)
+
   return (
-    <div>
-      home
+    <div className="home_page">
+      {blogs.map((blog) => (
+        <div key={blog._id}>
+          {blog.count > 0 && (
+            <>
+              <h3>
+                <Link to={`/blogs/${blog.name.toLowerCase()}`}>
+                  {blog.name} <small>( {blog.count} 投稿数)</small>
+                </Link>
+              </h3>
+              <hr className="mt-1" />
+              <div className="home_blogs">
+                {blog.blogs.map((blog) => (
+                  <CardVertically key={blog._id} blog={blog} />
+                ))}
+              </div>
+            </>
+          )}
+          {blog.count > 4 && (
+            <Link
+              className="text-end d-block mt-2 mb-3"
+              to={`/blogs/${blog.name}`}
+            >
+              続きを読む &gt;&gt;
+            </Link>
+          )}
+        </div>
+      ))}
     </div>
   )
 }
