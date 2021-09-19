cfe0b364dfe05c90aef4ef961410730ad36bff5f
igaue-takahhiko <hasewo.2610@gmail.com>
2021-09-19 19:44:18 +0900

    implememnt Reply comment UI

--- client/src/components/comments/CommentList.tsx
+++ client/src/components/comments/CommentList.tsx
@@ -1,11 +1,45 @@
-import React from "react"
-import { IComment } from "../../utils/globalTypes"
+import React, { useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { IComment, RootStore } from "../../utils/globalTypes"
+
+import Input from "./Input"
 
 interface IProps {
   comment: IComment
+  showReply: IComment[]
+  setShowReply: (showReply: IComment[]) => void
 }
 
-const CommentList: React.FC<IProps> = ({ comment }) => {
+const CommentList: React.FC<IProps> = ({
+  children,
+  comment,
+  showReply,
+  setShowReply,
+}) => {
+  const dispatch = useDispatch()
+  const { auth } = useSelector((state: RootStore) => state)
+
+  const [onReply, setOnReply] = useState(false)
+
+  const handleReply = (body: string) => {
+    if (!auth.user || !auth.access_token) {
+      return
+    }
+
+    const data = {
+      user: auth.user,
+      blog_id: comment.blog_id,
+      blog_user_id: comment.blog_user_id,
+      content: body,
+      reply_user: comment.user,
+      comment_root: comment._id,
+      createdAt: new Date().toISOString(),
+    }
+    console.log(data)
+    setShowReply([...showReply, data])
+    setOnReply(false)
+  }
+
   return (
     <div className="w-100">
       <div className="comment_box">
@@ -13,13 +47,20 @@ const CommentList: React.FC<IProps> = ({ comment }) => {
           className="p-2"
           dangerouslySetInnerHTML={{ __html: comment.content }}
         />
-        <div className="d-flex justify-content-between p-2">
-          <small style={{ cursor: "pointer" }}>- 返信 -</small>
+        <div className="p-2 d-flex justify-content-between">
+          <small
+            style={{ cursor: "pointer" }}
+            onClick={() => setOnReply(!onReply)}
+          >
+            {onReply ? "- キャンセル -" : "- 返信 -"}
+          </small>
+          <small className="m-2">
+            {new Date(comment.createdAt).toLocaleDateString()}
+          </small>
         </div>
-        <small className="m-2">
-          {new Date(comment.createdAt).toLocaleDateString()}
-        </small>
       </div>
+      {onReply && <Input callback={handleReply} />}
+      {children}
     </div>
   )
 }
